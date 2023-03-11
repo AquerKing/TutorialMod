223cf6f81cf59b1c525ed935edc81dca104b9182
Mr.King <aquerking@163.com>
2023-03-11 14:07:27 +0800

    Initial commit

---
+++ src/main/java/org/tutorial/tutorialmod/TutorialMod.java
@@ -0,0 +1,9 @@
+package org.tutorial.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+public class TutorialMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/org/tutorial/tutorialmod/client/TutorialModClient.java
@@ -0,0 +1,9 @@
+package org.tutorial.tutorialmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+public class TutorialModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
